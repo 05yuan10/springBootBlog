bc7d0f920b33112b20b05426db8095eb1b03bc97
05yuan10 <77374107+05yuan10@users.noreply.github.com>
2023-11-27 19:00:53 +0900

    level2 블로그 만들기: 글 등록, 수정

--- src/main/java/me/limyuan/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/limyuan/springbootdeveloper/controller/BlogViewController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,5 +36,20 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // 1. id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        // 게시글 생성하는 경우
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        }
+        // 게시글 수정하는 경우
+        else{
+            Article article = blogService.findById(id); // 기존 값을 가져온다.
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
